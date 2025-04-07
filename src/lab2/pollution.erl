4dd0cd756529e89093d1d27b12c6a19ffd1ac8e4
lythx <lythxxx@gmail.com>
2025-04-07 14:53:13 +0200

    lab2

--- src/lab2/pollution.erl
+++ src/lab2/pollution.erl
@@ -14,12 +14,13 @@
   get_one_value/4, get_station_mean/3, get_daily_mean/3, get_daily_min_and_max/3]).
 
 create_monitor() -> [].
+
 add_station(Name, Coords, Monitor) ->
   case get_station(Name, Monitor) of
     {error, _} ->
       case get_station(Coords, Monitor) of
         {error, _} ->
-          [Monitor | {Name, Coords, []}];
+          Monitor ++ [{Name, Coords, []}];
         _ ->
           {error, "Station with given coordinates already exists"}
       end;
@@ -37,12 +38,12 @@ add_value(Station, Time, Type, Value, Monitor) ->
           NewReadings =
             case proplists:get_value(Type, Readings) of
               undefined ->
-                [Readings | {Type, [{Time, Value}]}];
+                Readings ++ [{Type, [{Time, Value}]}];
               Values ->
                 NewValues = proplists:delete(Time, Values) ++ [{Time, Value}],
                 proplists:delete(Type, Readings) ++ [{Type, NewValues}]
             end,
-          add_station_with_readings(Name, Coords, NewReadings, remove_station(Name, Monitor));
+          remove_station(Name, Monitor) ++ [{Name, Coords, NewReadings}];
         _ ->
           {error, "Value already exists"}
       end
@@ -60,21 +61,21 @@ remove_value(Station, Time, Type, Monitor) ->
           Values = proplists:get_value(Type, Readings),
           NewValues = proplists:delete(Time, Values),
           NewReadings = proplists:delete(Type, Readings) ++ [{Type, NewValues}],
-          add_station_with_readings(Name, Coords, NewReadings, remove_station(Name, Monitor))
+          remove_station(Name, Monitor) ++ [{Name, Coords, NewReadings}]
       end
   end.
 
 get_one_value(Station, Time, Type, Monitor) ->
   case get_station(Station, Monitor) of
-    {error, _} ->
-      {error, _};
+    {error, Message} ->
+      {error, Message};
     {_, _, Readings} ->
       case proplists:get_value(Type, Readings) of
         undefined ->
           {error, "Value of given type does not exist"};
         Values ->
           case proplists:get_value(Time, Values) of
-            {error, Message} -> {error, Message};
+            undefined -> {error, "Value at given time does not exist"};
             Value -> Value
           end
       end
@@ -82,8 +83,8 @@ get_one_value(Station, Time, Type, Monitor) ->
 
 get_station_mean(Station, Type, Monitor) ->
   case get_station(Station, Monitor) of
-    {error, _} ->
-      {error, _};
+    {error, Message} ->
+      {error, Message};
     {_, _, Readings} ->
       case proplists:get_value(Type, Readings) of
         undefined ->
@@ -115,31 +116,23 @@ get_daily_mean(Type, Date, Monitor) ->
   end.
 
 get_daily_min_and_max(Type, Date, Monitor) ->
-  lists:foldl(
+  {DailyMin, DailyMax} = lists:foldl(
     fun({_, _, Readings}, {AccMin, AccMax}) ->
-      {Min, Max} = get_daily_min_and_max_for_readings(Readings, Type, Date),
-      NewMin =
-        case Min of
-          {error, _} -> AccMin;
-          Min ->
-            case AccMin of
-              undefined -> Min;
-              _ -> lists:min([Min, AccMin])
-            end
-        end,
-      NewMax =
-        case Max of
-          {error, _} -> AccMax;
-          Max ->
-            case AccMax of
-              undefined -> Max;
-              _ -> lists:max([Max, AccMax])
-            end
-        end,
-      {NewMin, NewMax}
+      case get_daily_min_and_max_for_readings(Readings, Type, Date) of
+        {error, _} -> {AccMin, AccMax};
+        {Min, Max} ->
+          case AccMin of
+            undefined -> {Min, Max};
+            _ -> {lists:min([Min, AccMin]), lists:max([Max, AccMax])}
+          end
+      end
     end,
     {undefined, undefined},
-    Monitor).
+    Monitor),
+  case {DailyMin, DailyMax} of
+    {undefined, undefined} -> {error, "No values of given type for given day"};
+    _ -> {DailyMin, DailyMax}
+  end.
 
 get_station(_, []) ->
   {error, "Station not found"};
@@ -153,15 +146,12 @@ get_station(Station, [_ | T]) ->
 remove_station(Name, Monitor) ->
   lists:filter(fun({N, _, _}) -> N =/= Name end, Monitor).
 
-add_station_with_readings(Name, Coords, Readings, Monitor) ->
-  [Monitor | {Name, Coords, Readings}].
-
 get_daily_sum_and_count_for_readings(Readings, Type, Date) ->
   case proplists:get_value(Type, Readings) of
     undefined ->
-      {error, "Value of given type does not exist"};
+      {0, 0};
     Values ->
-      DayValues = lists:filter(fun({D, _}) -> match_date(D, Date) end, Values),
+      DayValues = lists:filter(fun({{D, _}, _}) -> D == Date end, Values),
       Sum = lists:foldl(fun({_, V}, Acc) -> Acc + V end, 0, DayValues),
       {Sum, length(DayValues)}
   end.
@@ -171,10 +161,10 @@ get_daily_min_and_max_for_readings(Readings, Type, Date) ->
     undefined ->
       {error, "Value of given type does not exist"};
     Values ->
-      DayValues = lists:filter(fun({D, _}) -> match_date(D, Date) end, Values),
+      DayValues = lists:filter(fun({{D, _}, _}) -> D == Date end, Values),
       MappedValues = lists:map(fun({_, V}) -> V end, DayValues),
-      {lists:min(MappedValues), lists:max(MappedValues)}
+      case MappedValues of
+        [] -> {error, "No values of given type on fiven day"};
+        _ -> {lists:min(MappedValues), lists:max(MappedValues)}
+      end
   end.
-
-match_date({{Date, _}, _}, Date) -> true;
-match_date(_, _) -> false.
--- src/lab2/pollution_test.erl
+++ src/lab2/pollution_test.erl
@@ -22,6 +22,7 @@ add_station_test() ->
   M1 = pollution:create_monitor(),
   M2 = pollution:add_station("Stacja 1", {1,1}, M1),
   ?assertNotMatch({error, _}, M2),
+  ?assertMatch([_ | _], M2),
   ?assertMatch({error, _}, pollution:add_station("Stacja 1", {1,1}, M2)),
   ?assertMatch({error, _}, pollution:add_station("Stacja 1", {2,2}, M2)),
   ?assertMatch({error, _}, pollution:add_station("Stacja 2", {1,1}, M2)).
@@ -55,11 +56,12 @@ add_value_test() ->
 add_value_fail_test() ->
   M = pollution:add_station("Stacja 1", {1,1}, pollution:create_monitor()),
   Time = calendar:local_time(),
-  ?assertNotMatch({error, _}, pollution:add_value("Stacja 1", Time, "PM10", 46.3, M)),
-  ?assertMatch({error, _}, pollution:add_value("Stacja 1", Time, "PM10", 46.3, M)),
-  ?assertMatch({error, _}, pollution:add_value("Stacja 1", Time, "PM10", 36.3, M)),
-  ?assertMatch({error, _}, pollution:add_value({1,1}, Time, "PM10", 46.3, M)),
-  ?assertMatch({error, _}, pollution:add_value({1,1}, Time, "PM10", 36.3, M)).
+  M1 = pollution:add_value("Stacja 1", Time, "PM10", 46.3, M),
+  ?assertNotMatch({error, _}, M1),
+  ?assertMatch({error, _}, pollution:add_value("Stacja 1", Time, "PM10", 46.3, M1)),
+  ?assertMatch({error, _}, pollution:add_value("Stacja 1", Time, "PM10", 36.3, M1)),
+  ?assertMatch({error, _}, pollution:add_value({1,1}, Time, "PM10", 46.3, M1)),
+  ?assertMatch({error, _}, pollution:add_value({1,1}, Time, "PM10", 36.3, M1)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -181,7 +183,7 @@ get_daily_mean_test() ->
 
   ?assertMatch(15.0, pollution:get_daily_mean("PM10",{2023,3,27}, M2)),
   ?assertMatch(15.0, pollution:get_daily_mean("PM10",{2023,3,27}, M6)),
-  ?assertMatch(15.0, pollution:get_daily_mean("PM10",{2023,3,27}, M9)).
+  ?assertMatch(258.8, pollution:get_daily_mean("PM10",{2023,3,27}, M9)).
 
 
 
@@ -195,3 +197,30 @@ get_daily_mean_fail_test() ->
   ?assertMatch({error, _}, pollution:get_daily_mean("PM25",{2023,3,27}, M2)),
   ?assertMatch({error, _}, pollution:get_daily_mean("PM10",{2023,3,29}, M2)).
 
+get_daily_min_and_max_test() ->
+  M = pollution:add_station("Stacja 3", {3,3}, pollution:add_station("Stacja 2", {2,2}, pollution:add_station("Stacja 1", {1,1}, pollution:create_monitor()))),
+  M1 = pollution:add_value("Stacja 1", {{2023,3,27},{11,16,10}}, "PM10", 10, M),
+  M2 = pollution:add_value("Stacja 2", {{2023,3,27},{11,16,11}}, "PM10", 20, M1),
+  M3 = pollution:add_value("Stacja 1", {{2023,3,27},{11,16,12}}, "PM10", 10, M2),
+  M4 = pollution:add_value("Stacja 2", {{2023,3,27},{11,16,13}}, "PM10", 20, M3),
+
+  M5 = pollution:add_value("Stacja 1", {{2023,3,27},{11,16,14}}, "PM25", 100, M4),
+  M6 = pollution:add_value("Stacja 2", {{2023,3,27},{11,16,15}}, "PM25", 220, M5),
+
+  M7 = pollution:add_value("Stacja 1", {{2023,3,28},{11,16,16}}, "PM10", 2000, M6),
+  M8 = pollution:add_value("Stacja 2", {{2023,3,28},{11,16,17}}, "PM10", 3000, M7),
+
+  M9 = pollution:add_value("Stacja 3", {{2023,3,27},{11,16,18}}, "PM10", 1234, M8),
+
+  ?assertMatch({10, 20}, pollution:get_daily_min_and_max("PM10",{2023,3,27}, M2)),
+  ?assertMatch({10, 20}, pollution:get_daily_min_and_max("PM10",{2023,3,27}, M6)),
+  ?assertMatch({10, 1234}, pollution:get_daily_min_and_max("PM10",{2023,3,27}, M9)).
+
+get_daily_min_and_max_fail_test() ->
+  M = pollution:add_station("Stacja 2", {2,2}, pollution:add_station("Stacja 1", {1,1}, pollution:create_monitor())),
+  ?assertMatch({error, _}, pollution:get_daily_min_and_max("PM10",{2023,3,27}, M)),
+  M1 = pollution:add_value("Stacja 1", {{2023,3,27},{11,16,10}}, "PM10", 10, M),
+  M2 = pollution:add_value("Stacja 2", {{2023,3,27},{11,16,11}}, "PM10", 20, M1),
+
+  ?assertMatch({error, _}, pollution:get_daily_min_and_max("PM25",{2023,3,27}, M2)),
+  ?assertMatch({error, _}, pollution:get_daily_min_and_max("PM10",{2023,3,29}, M2)).
